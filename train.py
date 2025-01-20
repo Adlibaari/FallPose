d46e6a49aa9868a43c544e8bc990886e990d2c1d
Adlibaari <93728649+Adlibaari@users.noreply.github.com>
2025-01-20 08:31:34 +0700

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 def main():
     # Load a model
-    model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolo11n-pose.pt")  # load a pretrained model (recommended for training)
     
     # Train the model
     results = model.train(data="data.yaml", epochs=100, imgsz=1280)
